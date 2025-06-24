b8b2ea75410c5f73190244e04fca60cb4d8c8afd
mura333 <ms.mura@alumno.etec.um.edu.ar>
2025-06-24 12:24:25 -0300

    Etapa 1: Definición básica de la clase Fraccion

---
+++ app/src/main/kotlin/org/example/Fraccion.kt
@@ -0,0 +1,33 @@
+package org.example
+
+class Fraccion(numerador: Int, denominador: Int) {
+
+    var numerador: Int = numerador
+        get() = field
+        set(value) {
+            field = value
+        }
+
+    var denominador: Int = denominador
+        get() = field
+        set(value) {
+            if (value == 0) {
+                throw IllegalArgumentException("El denominador no puede ser cero")
+            }
+            field = value
+        }
+
+    init {
+        if (denominador == 0) {
+            throw IllegalArgumentException("El denominador no puede ser cero")
+        }
+    }
+
+    override fun toString(): String {
+        return "$numerador/$denominador"
+    }
+
+    fun mostrar() {
+        println(this.toString())
+    }
+}
